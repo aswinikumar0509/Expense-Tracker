546523def94c15a6d6591925100130d157054099
Aswini Kumar <aswini0509@gmail.com>
2024-12-02 22:22:45 +0530

    update deleate feature

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from .forms import ExpenseForm
 from .models import Expense
+from django.db.models import Sum
+import datetime
 
 # Create your views here.
 
@@ -12,12 +14,31 @@ def index(request):
             expense.save()
 
     expenses = Expense.objects.all()
-    total_expenses = expenses.aggregate(sum('amount'))
+    total_expenses = expenses.aggregate(Sum('amount'))
 
+    # logic to calculate 365 days expenses
+    last_year=datetime.date.today() - datetime.timedelta(days=365)
+    data=Expense.objects.filter(date__gt = last_year)
+    yearly_sum=data.aggregate(Sum('amount'))
+    # print(yearly_sum)
+
+    # logic to calculate 30 days expenses
+    last_month=datetime.date.today() - datetime.timedelta(days=30)
+    data=Expense.objects.filter(date__gt =last_month)
+    monthly_sum=data.aggregate(Sum('amount'))
+
+    # logic to calculate 7 days expenses
+    last_week=datetime.date.today() - datetime.timedelta(days=7)
+    data=Expense.objects.filter(date__gt = last_week)
+    weekly_sum=data.aggregate(Sum('amount'))
+
+
+
+    print(total_expenses)
     expense_form = ExpenseForm()
 
 
-    return render(request,'myapp/index.html',{'expense_form':expense_form,'expenses':expenses})
+    return render(request,'myapp/index.html',{'expense_form':expense_form,'expenses':expenses,'total_expenses':total_expenses,'yearly_sum':yearly_sum,'monthly_sum':monthly_sum,'weekly_sum':weekly_sum})
 
 def edit(request,id):
     expense = Expense.objects.get(id=id)
