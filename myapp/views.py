d8873a3827d82d48db167e44dc6e2241e3897313
Aswini Kumar <aswini0509@gmail.com>
2024-12-03 23:22:07 +0530

    edit feature

--- myapp/views.py
+++ myapp/views.py
@@ -32,13 +32,20 @@ def index(request):
     data=Expense.objects.filter(date__gt = last_week)
     weekly_sum=data.aggregate(Sum('amount'))
 
+    # logic to calculate the sum on a particular date
+
+    daily_sums = Expense.objects.filter().values('date').order_by('date').annotate(sum=Sum('amount'))
+    print(daily_sums)
+    categorical_sums = Expense.objects.filter().values('category').order_by('category').annotate(sum=Sum('amount'))
+    print(categorical_sums)
 
 
-    print(total_expenses)
+
+    # print(total_expenses)
     expense_form = ExpenseForm()
 
 
-    return render(request,'myapp/index.html',{'expense_form':expense_form,'expenses':expenses,'total_expenses':total_expenses,'yearly_sum':yearly_sum,'monthly_sum':monthly_sum,'weekly_sum':weekly_sum})
+    return render(request,'myapp/index.html',{'expense_form':expense_form,'expenses':expenses,'total_expenses':total_expenses,'yearly_sum':yearly_sum,'monthly_sum':monthly_sum,'weekly_sum':weekly_sum,'daily_sums':daily_sums,'categorical_sums':categorical_sums})
 
 def edit(request,id):
     expense = Expense.objects.get(id=id)
